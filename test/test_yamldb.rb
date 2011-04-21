06bac0f520ab1ade89c9925bb7d405fb37bc404e
Jens Färber <jfaerber@tzi.de>
2011-04-21 09:10:43 +0200

    init tests for yamldb

---
+++ test/test_yamldb.rb
@@ -0,0 +1,18 @@
+#require 't_helper'
+require 'test/unit'
+require 'fidius-common/yamldb'
+require 'sqlite3'
+require 'fileutils'
+
+class YamlDbTest < Test::Unit::TestCase
+  def test_export
+    FileUtils.rm_r 'export_test' if File.exists? 'export_test'
+    Dir.mkdir 'export_test'
+    FIDIUS::Common::Db.export('test_yamldb/database.yml', 'test_db', 'export_test', false)
+  end
+
+  def test_import
+    FIDIUS::Common::Db.import('test_yamldb/database.yml', 'test_db', 'export_test/test_db')
+    FileUtils.rm_r 'export_test' if File.exists? 'export_test'
+  end
+end
