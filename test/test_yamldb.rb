4fb581f6b34e0ecf536f3811701ed4ec6c27673f
Jens Färber <jfaerber@tzi.de>
2011-04-21 09:53:07 +0200

    remove test_db after test

--- test/test_yamldb.rb
+++ test/test_yamldb.rb
@@ -14,5 +14,6 @@ class YamlDbTest < Test::Unit::TestCase
   def test_import
     FIDIUS::Common::Db.import('test_yamldb/database.yml', 'test_db', 'export_test/test_db')
     FileUtils.rm_r 'export_test' if File.exists? 'export_test'
+    FileUtils.rm 'test_db.sqlite3' if File.exists? 'test_db.sqlite3'
   end
 end
