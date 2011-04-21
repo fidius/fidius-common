95c1b36b4f692ff5a5b688d2c4b9d898a2af141b
Jens Färber <jfaerber@tzi.de>
2011-04-21 10:27:37 +0200

    testing with rake test

--- test/test_yamldb.rb
+++ test/test_yamldb.rb
@@ -1,5 +1,5 @@
-#require 't_helper'
-require 'test/unit'
+require 't_helper'
+#require 'test/unit'
 require 'fidius-common/yamldb'
 require 'sqlite3'
 require 'fileutils'
@@ -8,11 +8,11 @@ class YamlDbTest < Test::Unit::TestCase
   def test_export
     FileUtils.rm_r 'export_test' if File.exists? 'export_test'
     Dir.mkdir 'export_test'
-    FIDIUS::Common::Db.export('test_yamldb/database.yml', 'test_db', 'export_test', false)
+    FIDIUS::Common::Db.export('test/test_yamldb/database.yml', 'test_db', 'export_test', false)
   end
 
   def test_import
-    FIDIUS::Common::Db.import('test_yamldb/database.yml', 'test_db', 'export_test/test_db')
+    FIDIUS::Common::Db.import('test/test_yamldb/database.yml', 'test_db', 'export_test/test_db')
     FileUtils.rm_r 'export_test' if File.exists? 'export_test'
     FileUtils.rm 'test_db.sqlite3' if File.exists? 'test_db.sqlite3'
   end
