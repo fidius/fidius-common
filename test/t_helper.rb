50a99237c7f1407159b2a7be0cfdf9335873a29e
Jens Färber <jfaerber@tzi.de>
2011-04-21 19:48:50 +0200

    temporarily uncommented simplecov because of seg fault

--- test/t_helper.rb
+++ test/t_helper.rb
@@ -1,3 +1,10 @@
-require 'simplecov'
-SimpleCov.start
+# Temporarily uncommented simplecov to let the tests in Jenkins pass
+#
+# It will raise a segmentation fault when 'rake test' is run
+# under the Debian/Ubuntu version of Ruby (1.9.2p0).
+# The problem has obviously been fixed between 1.9.2p0 and 1.9.2p35
+# because it runs fine here.
+#
+#require 'simplecov'
+#SimpleCov.start
 require 'test/unit'
