eb8b2c8ba5cb4f81d4ae07740630d33ce0456848
Jens Färber <jfaerber@tzi.de>
2011-04-21 18:52:08 +0200

    improved tests

--- test/test_yamldb.rb
+++ test/test_yamldb.rb
@@ -16,20 +16,24 @@ TEST_DB = File.join(BASE_DIR, 'test_db.sqlite3')
 include DbInstall
 
 class YamlDbTest < Test::Unit::TestCase
-  def test_export
+
+  def setup
     DbInstall.migrate(MIGRATIONS, BASE_DIR)
 
     FIDIUS::Common::Db.export(DB_CONFIG, 'test_db', BASE_DIR, false)
   end
 
+  def test_export
+    assert(File.exists?(EXPORT_DIR))
+    assert(File.exists?(File.join(EXPORT_DIR, 'test_db.yml')))
+    assert(File.exists?(File.join(EXPORT_DIR, 'schema.rb')))
+  end
+
   def test_import
     FIDIUS::Common::Db.import(DB_CONFIG, 'test_db', EXPORT_DIR)
+  end
 
-    assert_equal(1, Abc.all.size)
-    assert_equal(1, Def.all.size)
-    assert_equal('TestA', Abc.first.a)
-    assert_equal(2, Def.first.d)
-
+  def teardown
     FileUtils.rm_r EXPORT_DIR if File.exists? EXPORT_DIR
     FileUtils.rm TEST_DB if File.exists? TEST_DB
   end
